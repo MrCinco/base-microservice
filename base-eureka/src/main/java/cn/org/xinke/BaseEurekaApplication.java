c662f893807c2f1fa250ada4d968a44a1aabb5d5
cincos <xiaobenzhuxk@yeah.net>
2018-12-03 00:14:25 +0800

    Init commit

---
+++ base-eureka/src/main/java/cn/org/xinke/BaseEurekaApplication.java
@@ -0,0 +1,12 @@
+package cn.org.xinke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseEurekaApplication.class, args);
+    }
+}
---
+++ doc/readme.txt
@@ -0,0 +1,15 @@
+微服务基础服务父工程
+
+一、统一编码声明：使用UTF-8编码
+
+二、技术栈版本说明
+1.Java版本：JDK1.8
+2.SpringBoot版本：2.1.0.RELEASE
+3.SpringCloud版本：Finchley.SR2
+
+三、子工程说明
+1.base-config：微服务配置管理中心
+2.base-eureka：微服务注册管理中心
+3.base-hystrix：微服务监控中心
+4.base-turbina：微服务聚合服务监控中心
+5.base-zipkin：微服务跟踪分析中心
